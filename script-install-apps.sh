40fb1c5cbad587b48ef564f7306747359497a3bb
MateusBonacinaZ <mateus.bonacina.b@gmail.com>
2024-05-22 20:27:21 -0300

    Adicionando novos programas e corrigindo bugs

--- script-install-apps.sh
+++ script-install-apps.sh
@@ -7,11 +7,11 @@
 ## Atualizando os repositórios e pacotes ##
 echo "[YUM/DNF] ATUALIZANDO A LISTA DOS REPOSITÓRIOS..."
 sudo dnf update -y
-echo -e "[YUM/DNF] LISTA DOS REPOSITÓRIOS ATUALIZADA! \n"
+echo -e "[YUM/DNF] LISTA DOS REPOSITÓRIOS ATUALIZADA... \n"
 
 echo "[YUM/DNF] ATUALIZANDO OS PACOTES..."
 sudo dnf upgrade -y
-echo -e "[YUM/DNF] PACOTES ATUALIZADOS! \n"
+echo -e "[YUM/DNF] PACOTES ATUALIZADOS... \n"
 
 ## Listando o nome dos pacotes que serão instalados via YUM/DNF ##
 PACOTES_DNF=(
@@ -25,10 +25,10 @@ for NOME_PACOTE in ${PACOTES_DNF[@]}; do
 	if ! dnf list --installed | grep -q $NOME_PACOTE; then
 		echo "[YUM/DNF] O PACOTE $NOME_PACOTE ESTÁ SENDO INSTALADO..."
 		sudo dnf install $NOME_PACOTE
-		echo -e "[YUM/DNF] PACOTE $NOME_PACOTE JÁ INSTALADO! \n"
+		echo -e "[YUM/DNF] PACOTE $NOME_PACOTE JÁ INSTALADO... \n"
 	
 	else
-		echo -e "[YUM/DNF] PACOTE $NOME_PACOTE JÁ INSTALADO! \n"
+		echo -e "[YUM/DNF] PACOTE $NOME_PACOTE JÁ INSTALADO... \n"
 	fi
 done
 
@@ -47,6 +47,8 @@ PACOTES_FLATPAK=(
 	"com.rafaelmardojai.Blanket"
 	"com.obsproject.Studio"
 	"org.videolan.VLC"
+	"com.jetbrains.PyCharm-Community"
+	"com.google.Chrome"
 	)
 
 ## Criando loop na lista de pacotes e verificando se estão instalados, caso não esteja eles serão instalados ##
@@ -54,10 +56,10 @@ for NOME_PACOTE in ${PACOTES_FLATPAK[@]}; do
 	if ! flatpak list | grep -q $NOME_PACOTE; then
 		echo "[FLATPAK] O PACOTE $NOME_PACOTE ESTÁ SENDO INSTALADO..."
 		sudo flatpak install flathub $NOME_PACOTE -y
-		echo -e "[FLATPAK] PACOTE $NOME_PACOTE JÁ INSTALADO! \n"
+		echo -e "[FLATPAK] PACOTE $NOME_PACOTE JÁ INSTALADO... \n"
 	
 	else
-		echo -e "[FLATPAK] PACOTE $NOME_PACOTE JÁ INSTALADO! \n"
+		echo -e "[FLATPAK] PACOTE $NOME_PACOTE JÁ INSTALADO... \n"
 	fi
 done
 
@@ -66,24 +68,33 @@ done
 ## SOFTWARES PROPRIETÁRIOS ##
 
 #Configurando WGET
-DIRETORIO_DOWNLOAD="/home/bonacina/Downloads/Instaladores"
+DIRETORIO_DOWNLOAD="/home/$(whoami)/Downloads/Instaladores"
 
 echo "[MKDIR] CRIANDO DIRETÓRIO DE SALVAMENTO DOS INSTALADORES..."
 mkdir $DIRETORIO_DOWNLOAD
-echo -e "[MKDIR] DIRETÓRIO OK! \n"
+echo -e "[MKDIR] DIRETÓRIO OK... \n"
 
 
 ## Listando o link dos pacotes que serão baixados pelo wget e instalados pelo rpm ##
 LINKS_PACOTES=(
-	"https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code-1.63.2-1639562596.el7.x86_64.rpm"
+	"https://vscode.download.prss.microsoft.com/dbazure/download/stable/dc96b837cf6bb4af9cd736aa3af08cf8279f7685/code-1.89.1-1715060595.el8.x86_64.rpm"
 	)
 
+## Verificamos se um determinado pacote já está instalado, caso não esteja ele será instalado ##
 for LINK_PACOTE in ${LINKS_PACOTES[@]}; do
-	echo "[WGET] FAZENDO O DOWNLOAD DE UM PACOTE..."
-	wget $LINK_PACOTE -P $DIRETORIO_DOWNLOAD
-	echo -e "[WGET] DOWNLOAD CONCLUÍDO! \n"
+	RPM_PACOTE="${LINK_PACOTE##*/}"
+	RPM_PACOTE="${RPM_PACOTE%.*}"
+	echo $RPM_PACOTE
+	if ! rpm -qa | grep -q $RPM_PACOTE; then
+		echo "[WGET] FAZENDO O DOWNLOAD DO PACOTE $LINK_PACOTE..."
+		wget $LINK_PACOTE -P $DIRETORIO_DOWNLOAD
+		echo -e "[WGET] DOWNLOAD CONCLUÍDO... \n"
+		echo -e "[RPM] INSTALANDO PACOTE... \n"
+		sudo rpm -i $DIRETORIO_DOWNLOAD/*.rpm
+	else
+		echo "[RPM] PACOTE $RPM_PACOTE JÁ INSTALADO..."
+	fi
 done
 
-sudo rpm -i $DIRETORIO_DOWNLOAD/*.rpm
 
 echo -e "\nPROGRAMA PÓS-INSTALAÇÃO EXECUTADO COM SUCESSO!\n\n"
